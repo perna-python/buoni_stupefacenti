861d1218e9436180581f5019c7d423dab1a05e07
Stefano <stecoding@gmail.com>
2024-07-08 16:37:50 +0200

    aggiunto legger e miglioramento codice

--- buoni_stupefacenti/main.py
+++ buoni_stupefacenti/main.py
@@ -5,6 +5,7 @@ import os
 import platform
 from datetime import date
 from pathlib import Path
+import logging
 
 import flet as ft
 from pdf import creaPdf
@@ -12,13 +13,18 @@ from pdf import creaPdf
 from dotenv import load_dotenv
 load_dotenv()
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
 def last_day_of_month(any_day: datetime.date):
+    logger.info(f"Calcolo l'ultimo giorno per il mese di {any_day}")
     next_month = any_day.replace(day=28) + datetime.timedelta(days=4)
     primoGiorno = datetime.datetime.strftime(any_day, "%d/%m/%Y")
     ultimoGiorno = datetime.datetime.strftime(next_month - datetime.timedelta(days=next_month.day), "%d/%m/%Y")
     return primoGiorno, ultimoGiorno
 
 def loginFarmarete():
+    logger.info("Login in Farmarete")
     with httpx.Client(timeout=None, follow_redirects=True, verify=False) as client:
         response = client.get("https://www.farmarete.it/api/shared/AppVersion")
         version = response.json()["version"]
@@ -33,22 +39,22 @@ def loginFarmarete():
         }
 
         response = client.post('https://www.farmarete.it/api/auth/login', json=json_data)
-
-        print(client.headers, cookies, response.json())
+        logger.info("Login response: %s", response.json())
 
     return client.headers, cookies, response.json()
 
 def idBuoniAcquisto(client:httpx.Client, anno:int, mese:int, cookies, headers):
+    logger.info(f"Ricerca degli ID degli ordini dell'anno {anno}, mese {mese}")
     primoGiorno, ultimoGiorno = last_day_of_month(datetime.date(anno, mese, 1))
     url = f"https://www.farmarete.it/api/stupeOrdini/elencoordini?pageSize=50&currentPage=0&sortField=dataInserimento&sortDirection=desc&searchText=&searchDateFrom={primoGiorno}&searchDateTo={ultimoGiorno}&searchStato=3,5,8,6,2,9,10&searchMagazzini=2,3&idFarmaciaAdminFiltro=0"
     response = client.get(url, cookies=cookies, headers=headers)
-    print(response)
     ordiniEffettuati = response.json()
     idOrdine = [str(i["idOrdine"]) for i in ordiniEffettuati["items"]]
-    print(ordiniEffettuati["nItems"], idOrdine)
+    logger.info(f"Numero di ordini: {ordiniEffettuati["nItems"]}, ID ordini: {idOrdine}")
     return idOrdine, ordiniEffettuati["nItems"]
 
 def creaCartella(base_path: Path, anno: str, mese: str):
+    logger.info(f"Creazione cartella per anno: {anno}, mese: {mese}")
     path_cartella = base_path / anno / mese
     path_cartella.mkdir(parents=True, exist_ok=True)
     (path_cartella / "ordineFirmato").mkdir(exist_ok=True)
@@ -56,14 +62,17 @@ def creaCartella(base_path: Path, anno: str, mese: str):
     return path_cartella
 
 def download(pathPDF: Path, response):
+    logger.info(f"Download file in {pathPDF}")
     with open(pathPDF, "wb") as f:
         f.write(response.content)
     
 def aperturaFile(pathCartella: Path, nomeFile: str):
+    logger.info(f"Apertura del file {nomeFile}")
     pathFile = pathCartella / nomeFile
     subprocess.Popen([str(pathFile)], shell=True)
 
 def buoniAcquisto(anno: int, mese: int, openFile=False):
+    logger.info(f"Processando gli ordine dell' anno {anno}, mese {mese}")
     headers, cookies, token = loginFarmarete()
     base_path = Path(os.getcwd()) / "documenti"
     with httpx.Client(timeout=None, follow_redirects=True, verify=False, headers=headers, cookies=cookies) as client:
@@ -72,6 +81,7 @@ def buoniAcquisto(anno: int, mese: int, openFile=False):
         pathCartella = creaCartella(base_path, str(anno), str(mese))
 
         for ordine_id in idOrdine:
+            logger.info(f"Processando l'ID ordine {ordine_id}")
             url = f"https://www.farmarete.it/api/shared/getUseOnceToken?contentId={ordine_id}"
             response = client.get(url, cookies=cookies, headers=headers)
             token = response.json()["token"]
@@ -91,8 +101,10 @@ def buoniAcquisto(anno: int, mese: int, openFile=False):
         pathPDFUnito = creaPdf(anno=str(anno), mese=str(mese), base_path=base_path)
 
         if platform.system() == "Windows":
+            logger.info("Apertura file PDF unito su Windows")
             subprocess.Popen([str(pathPDFUnito)], shell=True)
         else:
+            logger.info("Apertura file PDF unito su Mac")
             subprocess.call(["open", str(pathPDFUnito)])
 
         if openFile:
@@ -103,6 +115,7 @@ def buoniAcquisto(anno: int, mese: int, openFile=False):
             # apertura file contro Firmato
             for file in (pathCartella / "ordineControFirmato").glob("*.pdf"):
                 aperturaFile(pathCartella=pathCartella / "ordineControFirmato", nomeFile=file.name)
+                
 def main(page: ft.Page):
     def ordini(e):
         buoniAcquisto(anno=int(annoTextField.value), mese=int(meseTextField.value))
--- buoni_stupefacenti/pdf.py
+++ buoni_stupefacenti/pdf.py
@@ -1,8 +1,13 @@
 import PyPDF2
-import os
 from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 def creaPdf(anno: str, mese: str, base_path: Path):
+    logger.info(f"Creazione PDF per l'anno: {anno}, mese: {mese}")
+
     # Percorsi delle cartelle
     contro_firmato_path = base_path / anno / mese / "ordineControFirmato"
     firmato_path = base_path / anno / mese / "ordineFirmato"
@@ -10,19 +15,15 @@ def creaPdf(anno: str, mese: str, base_path: Path):
     # Liste dei file PDF
     listaPdfControFirmato = [file for file in contro_firmato_path.iterdir() if file.suffix == ".pdf"]
     listaPdfFirmato = [file for file in firmato_path.iterdir() if file.suffix == ".pdf"]
-    listaPdf = sorted(listaPdfControFirmato + listaPdfFirmato)
-
-    # Lista dei percorsi dei file PDF
-    listaPdfPath = [
-        file if "ControFirmato" in file.stem else file
-        for file in listaPdf
-    ]
+    listaPdfPath = sorted(listaPdfControFirmato + listaPdfFirmato, key=lambda file: file.name)
+    logger.info(f"Trovati {len(listaPdfFirmato)} firmati e {len(listaPdfControFirmato)} contro firmati")
 
     # Crea un oggetto PdfFileWriter per scrivere il nuovo file PDF
     pdfWriter = PyPDF2.PdfWriter()
 
     # Aggiungi pagine dai file PDF di origine al nuovo file
     for file_path in listaPdfPath:
+        logger.info(f"Aggiungo pagina da {file_path}")
         with open(file_path, "rb") as file:
             reader = PyPDF2.PdfReader(file)
             for page in reader.pages:
@@ -32,5 +33,6 @@ def creaPdf(anno: str, mese: str, base_path: Path):
     output_file_path = base_path / anno / mese / 'stupefacenti.pdf'
     with open(output_file_path, 'wb') as outputFile:
         pdfWriter.write(outputFile)
+        logger.info(f"PDF unificato creato in {output_file_path}")
 
     return output_file_path
